ef3b6ede6ee3574040560a91ab4100ba1a40ba3c
Wesley V. Lima <wesley.vieira.lima.1999@gmail.com>
2017-11-06 15:38:58 -0200

    Incompleto

--- db/db_rc.sql
+++ db/db_rc.sql
@@ -7,18 +7,19 @@ CREATE TABLE livros(
 	titulo NVARCHAR(100) NOT NULL,
     autor NVARCHAR(40) NOT NULL,
     editora NVARCHAR(30) NOT NULL,
+    imgCapa NVARCHAR(30) DEFAULT 'without' NOT NULL,
     anoEdicao YEAR NOT NULL,
     
     PRIMARY KEY (id_livro)
 );
 
 INSERT INTO livros
-(isbn, titulo, autor, editora, anoEdicao) VALUES
-('9788599296493','O Nome do Vento - A Crônica do Matador Rei - Primeiro Dia','Patrick Rothfuss', 'Arqueiro', 2009),
-('9788580410327','O Temor do Sábio – a Crônica do Matador do Rei - Segundo Dia','Patrick Rothfuss', 'Arqueiro', 2011),
-('9788580415506', 'Mago - Aprendiz', 'Raymond E. Feist', 'Arqueiro', 2016),
-('9788565765695', 'A Rainha Vermelha', 'Victoria Aveyard', 'Seguinte', 2015),
-('9788580415216','O Feiticeiro de Terramar - Ciclo Terramar', 'Ursula K. Le Guin', 'Arqueiro', 1968),
-('9788576573005','Neuromancer', 'Gibson, William', 'ALEPH', 2016);
+(isbn, titulo, autor, editora, imgCapa, anoEdicao) VALUES
+('9788599296493','O Nome do Vento - A Crônica do Matador Rei - Primeiro Dia','Patrick Rothfuss', 'onomedovento', 'Arqueiro', 2009),
+('9788580410327','O Temor do Sábio – a Crônica do Matador do Rei - Segundo Dia','Patrick Rothfuss', default,'Arqueiro', 2011),
+('9788580415506', 'Mago - Aprendiz', 'Raymond E. Feist', 'Arqueiro',default, 2016),
+('9788565765695', 'A Rainha Vermelha', 'Victoria Aveyard', 'Seguinte',default, 2015),
+('9788580415216','O Feiticeiro de Terramar - Ciclo Terramar', 'Ursula K. Le Guin', 'Arqueiro', default,1968),
+('9788576573005','Neuromancer', 'Gibson, William', 'ALEPH', default,2016);
 
 select * from livros;
