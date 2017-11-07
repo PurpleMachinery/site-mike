58534908add2c00f3744ca82f7a6f1206f57d63e
wesley vieira <wesley.vieira.lima.1999@gmail.com>
2017-11-06 22:40:01 -0200

    selects feitos

--- db/db_rc.sql
+++ db/db_rc.sql
@@ -13,13 +13,13 @@ CREATE TABLE livros(
     PRIMARY KEY (id_livro)
 );
 
+ALTER DATABASE red_box CHARSET = UTF8 COLLATE = utf8_general_ci;
+
 INSERT INTO livros
-(isbn, titulo, autor, editora, imgCapa, anoEdicao) VALUES
+(isbn, titulo, autor, imgCapa, editora, anoEdicao) VALUES
 ('9788599296493','O Nome do Vento - A Crônica do Matador Rei - Primeiro Dia','Patrick Rothfuss', 'onomedovento', 'Arqueiro', 2009),
 ('9788580410327','O Temor do Sábio – a Crônica do Matador do Rei - Segundo Dia','Patrick Rothfuss', default,'Arqueiro', 2011),
 ('9788580415506', 'Mago - Aprendiz', 'Raymond E. Feist', 'Arqueiro',default, 2016),
 ('9788565765695', 'A Rainha Vermelha', 'Victoria Aveyard', 'Seguinte',default, 2015),
 ('9788580415216','O Feiticeiro de Terramar - Ciclo Terramar', 'Ursula K. Le Guin', 'Arqueiro', default,1968),
 ('9788576573005','Neuromancer', 'Gibson, William', 'ALEPH', default,2016);
-
-select * from livros;
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -16,7 +17,40 @@
 			<a href="#"><span class="spanIcone margemUp glyphicon glyphicon-cloud"></span></a>
 		</div>
 		<div class="livro">
-			
+			<center>
+				<?php  
+					# Substitua abaixo os dados, de acordo com o banco criado
+					$user = "root"; 
+					$password = ""; 
+					$database = "red_box"; 
+					 
+					# O hostname deve ser sempre localhost 
+					$hostname = "localhost"; 
+					 
+					# Conecta com o servidor de banco de dados 
+					$con=mysqli_connect( $hostname, $user, $password ) or die( ' Erro na conexão ' ); 
+					 
+					# Seleciona o banco de dados 
+					mysqli_select_db(  $con ,$database) or die( 'Erro na seleção do banco' );
+					 
+					# Executa a query desejada 
+					$query = "SELECT * FROM livros"; 
+					$result_query = mysqli_query($con, $query) or die(' Erro na query:' . $query . ' ' . mysqli_error() ); 
+					 
+					# Exibe os registros na tela 
+					while ($row = mysqli_fetch_array($result_query)) { 
+						print "<div class='bloco'>".
+									"<hgroup>".
+										"<img src='img/capas/$row[imgCapa].jpg'>".
+										"<h2>$row[titulo]</h2>".
+										"<h3>$row[autor]</h3>".
+										"<h4>$row[isbn]</h4>". 
+									"</hgroup>" .
+									"<br/>".
+								"</div>";
+					}					 
+				?>
+			</center>		
 		</div>
 	</div>
 	</body>
